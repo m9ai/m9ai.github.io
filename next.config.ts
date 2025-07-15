84c53e1245298bd51675f8d720d010ac4acde7c6
张舰 <zhangjian.chandan@bytedance.com>
2025-07-15 21:49:46 +0800

    fix: basePath invalid

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   // 如果部署到 https://<username>.github.io/<repo-name>/，请取消注释并替换为你的仓库名称
-  basePath: process.env.NODE_ENV !== 'development' ? '/m9ai.github.io' : '',
+  // basePath: process.env.NODE_ENV !== 'development' ? '/m9ai.github.io' : '',
   images: {
     unoptimized: true
   }
