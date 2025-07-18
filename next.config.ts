5c6a0286230f4d07d84591099576dcf53bf31fe4
chandan <zhangjianzy@qq.com>
2025-07-18 16:48:39 +0800

    chore: update next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 import createMDX from '@next/mdx'
 import withPWA from 'next-pwa'
 
+import path from 'path';
+
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
+
   output: 'export',
   // 如果部署到 https://<username>.github.io/<repo-name>/，请取消注释并替换为你的仓库名称
   // basePath: process.env.NODE_ENV !== 'development' ? '/m9ai.github.io' : '',
