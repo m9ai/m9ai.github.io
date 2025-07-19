d5ef5f5cd1a5b388a0b83edfad5d3ccd5d6616a8
chandan <zhangjianzy@qq.com>
2025-07-19 13:41:51 +0800

    fix invalid clarity id

--- src/app/components/ClarityInit.tsx
+++ src/app/components/ClarityInit.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function ClarityInit() {
     useEffect(() => {
         if (typeof window !== "undefined") {
-            Clarity.init(process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID as string);
+            Clarity.init('sh3gwrtagw');
         }
     }, []);
 
